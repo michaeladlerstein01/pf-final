82ae7b253a6bce8402a45412dc2bcb4f2090708e
michaeladlerstein01 <74989922+michaeladlerstein01@users.noreply.github.com>
2022-03-21 11:46:31 +0000

    Add files via upload

--- move_robot.py
+++ move_robot.py
@@ -10,11 +10,13 @@ import pygame
 import math 
 import random as r
 from scipy.stats import norm
+import matplotlib.pyplot as plt
 
 
 green = (0 , 255 , 0)
 red = (255 , 0 , 0)
 blue = (0 ,0 , 255)
+estimcol = (100 , 100 , 255)
 white = (255 ,255 , 255)
 col  = (252, 3, 219)
 black = (0 ,0 ,0)
@@ -22,9 +24,8 @@ black = (0 ,0 ,0)
 pygame.init()
 dimx , dimy = 800 , 600
 surface = pygame.display.set_mode((dimx , dimy))
-manholes= [(100 , 200) , (300 , 400) , (400 , 200) , (500 , 500) , (200 , 500) , (206 , 200) , (300 , 80), (100 , 100) , (200  , 150) , (150 , 200) , (200  , 150) , (300 , 350) ]
+manholes= [(100 , 200) , (300 , 400) , (400 , 200) , (500 , 505) , (200 , 500) , (206 , 200) , (300 , 80), (100 , 100) , (200  , 150) , (150 , 200) , (200  , 150) , (300 , 350) ]
               
-
         
 def plot_manholes(manholes) :
     for point in manholes :
@@ -36,7 +37,6 @@ def plot_manholes(manholes) :
 starting_point =  manholes[0]
 location = starting_point
 n = manholes.index(location)
-
 
 
 def move(location,  n):
@@ -82,7 +82,7 @@ number_particles = 100
 
 particle_location = location
 particle_angle = []
-particle_list = []
+particle_listi = []
 
 
 def move_particles(particle_location):        
@@ -90,10 +90,9 @@ def move_particles(particle_location):
         x = manholes[n+1][0] - manholes[n][0]
         y = manholes[n+1][1] - manholes[n][1]  
         grad = y/x  
-        angle_particle = np.arctan(y/x) + np.random.normal(0, 0.1) 
+        angle_particle = np.arctan(y/x) + np.random.normal(0, 1) 
         particle_angle.append(angle_particle)
-        
-        step_particle = np.random.normal(5, 1 , number_particles)    
+        step_particle = np.random.normal(5, 3 , number_particles)    
         if grad != 0 and manholes[n+1][0] - manholes[n][0] > 0 :                                                          
             locationx = particle_location[0] + np.cos(angle_particle)*step_particle
             locationy = particle_location[1] + np.sin(angle_particle)*step_particle
@@ -114,20 +113,20 @@ def move_particles(particle_location):
             locationy = particle_location[1] - np.sin(angle_particle)*step_particle
             particle_location = (np.round(locationx , 2), np.round(locationy , 2))
         
-        particle_list.append(particle_location)  
+        particle_listi.append(particle_location)  
         return particle_location 
      
 
 def particle_sense(particle_location , n ):
     x1 = particle_location[0]
     y1 = particle_location[1]
-
     particle_distance = np.sqrt((manholes[n+1][0] - particle_location[0])**2 + (manholes[n+1][1] - particle_location[1])**2) + np.random.normal(0, 2) 
     return particle_distance 
 
-weight_particle = []
+
 def weight(distance , particle_distance):
     weight_particle  = norm.pdf(particle_distance, distance, 2 )
+    
     return weight_particle
 
 
@@ -138,16 +137,33 @@ def resample(weight_particle , particle_location ):
     particle_location = list(zip(*resample))
     return particle_location
          
+def estimated_location(particle_location):
+    estimatex = np.mean(particle_location[0])
+    estimatey = np.mean(particle_location[1])
+    pygame.draw.circle(surface, estimcol , (estimatex, estimatey) , 3)  
+    #rmse = np.sqrt(estimatex^2 - estimatey^2)
+    return estimatex , estimatey #, rmse
 
 
-#def sample()
+xplt = []
+yplt = []   
+rmse = []
+def data(estimatex , estimatey , location):
+    xval = location[0] - estimatex
+    yval = location[1] - estimatey
+    yplt.append(yval) 
+    xplt.append(xval)
+    error = np.sqrt(yval**2 + xval**2)
+    rmse.append(error)
+    return xplt , yplt , rmse
            
     
 while True : 
     for event in pygame.event.get() : 
         if event.type == pygame.QUIT :               
             pygame.quit()
-            quit()      
+            quit()    
+            
     if event.type == pygame.KEYDOWN:        
         n , location , distance  = sense(location , n  )                     
         if n == length:    
@@ -158,30 +174,27 @@ while True :
         else :            
             location  = move( location, n)   
             pygame.draw.circle(surface, red, (location[0]  , location[1]) , 6)
-
             particle_location  = move_particles(particle_location) 
-            
-            for i in range(number_particles-1):
+            for i in range(number_particles):
                 x = particle_location[0][i]
                 y = particle_location[1][i]
                 pygame.draw.circle(surface, col, (x , y)  , 2) 
-            
             particle_distance = particle_sense(particle_location , n )
-           
             weight_particle = weight(distance , particle_distance)
-            if any(weight_particle) == True:
-           # print(weight_particle)
-                particle_location =  resample(weight_particle , particle_location )
-            #weight_particle = []
-            
-            
 
-#=============================================================================   
+            if any(weight_particle) == True:
+                    particle_location =  resample(weight_particle , particle_location )
+
+            estimatex , estimatey = estimated_location(particle_location)
+            xplt , yplt , rmse  = data(estimatex , estimatey , location)
+            plt.plot(xplt) 
+            plt.plot(yplt)
+            plt.plot(rmse) 
+            pygame.draw.circle(surface, estimcol, (estimatex , estimatey) , 6)
     else :
         pygame.draw.circle(surface, red, (location[0]  , location[1]) , 6)  
-        
-        
-#========================================================                    
+          
+               
     pygame.display.update()         
     surface.fill(black)                               
     pygame.time.delay(100)                  
